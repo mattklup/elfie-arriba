253fd905a85627ffbf30ed0fb8412887588ff5a8
Eric Maino <ericmai@microsoft.com>
2020-08-12 11:58:33 -0700

    FEATURE: Enable disbling crawler for base image

--- .docker/Crawler/run-crawler.sh
+++ .docker/Crawler/run-crawler.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
-while [ -z ${CRAWLER_DISABLED} ]
-do
-   echo dotnet Arriba.WorkItemCrawler.dll $@
-   dotnet Arriba.WorkItemCrawler.dll $@
-   sleep 300
-done
+RUN_CRAWLER=1
+
+if [ ! -z ${CRAWLER_DISABLED} ];
+then
+   RUN_CRAWLER=0
+
+   if [ "${CRAWLER_DISABLED}" = "0" ] || [ "${CRAWLER_DISABLED}" = "false" ];
+   then
+      RUN_CRAWLER=1
+   fi
+fi
+
+if [ "${RUN_CRAWLER}" = "1" ]
+then
+   while :;
+   do
+      echo dotnet Arriba.WorkItemCrawler.dll $@
+      dotnet Arriba.WorkItemCrawler.dll $@
+      sleep 300
+   done   
+else
+   echo "CRAWLER DISABLED"
+fi
+
