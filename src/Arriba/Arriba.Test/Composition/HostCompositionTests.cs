92f44531ae0d6fe2bada48ab61d6755b88353631
Eric Maino <eric.maino@microsoft.com>
2020-08-14 16:31:05 -0700

    TEST: Adding a test to verify healthy composition

---
+++ src/Arriba/Arriba.Test/Composition/HostCompositionTests.cs
@@ -0,0 +1,19 @@
+﻿using Arriba.Server;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Arriba.Test.Composition
+{
+    [TestClass]
+    public class HostCompositionTests
+    {
+        [TestMethod]
+        public void VerifiedApplicationServerComposition()
+        {
+            var host = new Arriba.Server.Hosting.Host();
+            host.Compose();
+
+            var server = host.GetService<ComposedApplicationServer>();
+            Assert.IsNotNull(server);
+        }
+    }
+}
