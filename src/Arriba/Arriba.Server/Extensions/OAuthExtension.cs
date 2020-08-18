572edc3b1e71e070578ba9abaca3edabc49f65b8
Vinicius Souza <visouza@microsoft.com>
2020-08-18 16:58:35 -0700

    FEATURE: Add OAuthExtension

---
+++ src/Arriba/Arriba.Server/Extensions/OAuthExtension.cs
@@ -0,0 +1,36 @@
+﻿using Arriba.Security.OAuth;
+using Arriba.Server.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Arriba.Server.Extensions
+{
+    public static class OAuthExtension
+    { 
+        public static IServiceCollection AddOAuth(this IServiceCollection services, IArribaServerConfiguration serverConfig)
+        {
+            if (!serverConfig.EnabledAuthentication)
+                return services;
+            
+            var azureTokens = AzureJwtTokenFactory.CreateAsync(serverConfig.OAuthConfig).Result;
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(azureTokens.Configure);
+
+            //Check What I have at the user after this middleware.
+            var jwtBearerPolicy = new AuthorizationPolicyBuilder()
+               .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+               .RequireAuthenticatedUser()
+               .Build();
+
+            services.AddAuthorization(auth =>
+            {
+                auth.DefaultPolicy = jwtBearerPolicy;
+            });
+
+            services.AddSingleton((_) => serverConfig.OAuthConfig);
+
+            return services;
+        }
+    }
+}
