575464dff53b0cc9cfe31ff87cfc162d98aa6de7
Vinicius Souza <visouza@microsoft.com>
2020-08-31 14:49:02 -0700

    FIX: Initializing ArribaServices

--- src/Arriba/Arriba.Composition/Composition/Host.cs
+++ src/Arriba/Arriba.Composition/Composition/Host.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
@@ -29,6 +29,8 @@ namespace Arriba.Composition
     {
         public static void AddArribaServices(this IServiceCollection services, ISecurityConfiguration config)
         {
+            ArribaServices.Initialize();
+
             services.AddSingleton<ISecurityConfiguration>(config);
          
             services.AddContentReadersWriters();
